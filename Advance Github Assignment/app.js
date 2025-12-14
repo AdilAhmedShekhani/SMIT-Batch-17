ce75860448427d76f59f9274798729c810506523
Ahmed-Devx <ahmedoffice69@gmail.com>
2025-12-14 13:43:47 +0500

    palindrome checker assignment comleted

--- Advance Github Assignment/app.js
+++ Advance Github Assignment/app.js
@@ -1,4 +1,19 @@
-// Write a Statemnet of Palindrom without using any method
-// Create Pull reques then add your code
-// Your Code Here 
+var input = document.querySelector("input");
+var result = document.getElementById("result");
 
+input.onkeyup = function () {
+  var word = input.value;
+  var reverse = "";
+
+  for (var i = word.length - 1; i >= 0; i--) {
+    reverse = reverse + word[i];
+  }
+
+  if (word === "") {
+    result.innerHTML = "";
+  } else if (word === reverse) {
+    result.innerHTML = "Palindrome";
+  } else {
+    result.innerHTML = "Not Palindrome";
+  }
+};
