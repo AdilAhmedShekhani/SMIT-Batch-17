1905f43ea59b8220bcd2cc09573059fbb43fa935
UmerBinKamran <umerbinkamran@gmail.com>
2025-12-14 23:26:33 +0500

    Commit Complete

--- Advance Github Assignment/app.js
+++ Advance Github Assignment/app.js
@@ -1,4 +1,17 @@
-// Write a Statemnet of Palindrom without using any method
-// Create Pull reques then add your code
-// Your Code Here 
+function checkPalindrome() {
+  var word = document.getElementById("wordInput").value;
+  var arr = [word];
+  var word2 = "";
 
+  for (var i = arr[0].length - 1; i >= 0; i--) {
+    word2 += arr[0][i];
+  }
+
+  if (arr[0] === word2) {
+    document.getElementById("result").innerHTML =
+      "Yes, " + arr[0] + " is a Palindrome";
+  } else {
+    document.getElementById("result").innerHTML =
+      arr[0] + " is Not a Palindrome";
+  }
+}
