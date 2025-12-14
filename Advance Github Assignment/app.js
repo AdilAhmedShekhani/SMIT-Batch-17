728ad81d741b7fa3be5798d294714d162f4aab07
Aneel84 <aneeltamsani@gmail.com>
2025-12-14 20:35:11 +0500

    palindrome checker

--- Advance Github Assignment/app.js
+++ Advance Github Assignment/app.js
@@ -2,3 +2,25 @@
 // Create Pull reques then add your code
 // Your Code Here 
 
+
+function checkPalindrome(){
+var str=document.getElementById("word").value;;
+var isPalindrome=true;
+var len=0;
+while(  str[len]!==undefined){
+
+len++;
+}
+for(var i=0; i<len/2; i++){
+    if(str[i]!==str[len-1-i]){
+        isPalindrome=false;
+        break;
+    }
+}
+if(isPalindrome){
+ document.getElementById("result").innerText = str + " is a palindrome";
+}   
+else{
+document.getElementById("result").innerText = str + " is a  not palindrome";
+}
+}
