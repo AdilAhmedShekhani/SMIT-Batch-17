39dbca04d8d31954ac45d37899819ad3910424a1
Muhammad-Huzaifa-Coding <m.huzaif999@gmail.com>
2026-02-24 16:42:22 +0500

    Added Palindrome Checker in Advance Github Assignment

--- Advance Github Assignment/app.js
+++ Advance Github Assignment/app.js
@@ -2,3 +2,21 @@
 // Create Pull reques then add your code
 // Your Code Here 
 
+let input = document.querySelector("input");
+
+input.addEventListener("keyup", function () {
+
+    let original = input.value;
+    let reverse = "";
+
+    for (let i = original.length - 1; i >= 0; i--) {
+        reverse += original[i];
+    }
+
+    if (original === reverse && original !== "") {
+        console.log("Palindrome");
+    } else {
+        console.log("Not Palindrome");
+    }
+
+});
