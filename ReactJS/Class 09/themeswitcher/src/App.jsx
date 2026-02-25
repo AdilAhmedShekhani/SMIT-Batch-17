a21ea21f06623f851a7a304ba554242e00aebefe
AdilAhmedShekhani <adilaziz2009@live.com>
2026-02-25 22:44:17 +0500

    Wrap ThemeProvider

--- ReactJS/Class 09/themeswitcher/src/App.jsx
+++ ReactJS/Class 09/themeswitcher/src/App.jsx
@@ -1,16 +1,19 @@
 import "./App.css";
+import { ThemeProvider } from "./contexts/theme";
 
 function App() {
   return (
-    <div className="flex flex-wrap min-h-screen items-center">
-      <div className="w-full">
-        <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
-          {/* theme btn */}
-        </div>
+    <ThemeProvider>
+      <div className="flex flex-wrap min-h-screen items-center">
+        <div className="w-full">
+          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+            {/* theme btn */}
+          </div>
 
-        <div className="w-full max-w-sm mx-auto">{/* Card */}</div>
+          <div className="w-full max-w-sm mx-auto">{/* Card */}</div>
+        </div>
       </div>
-    </div>
+    </ThemeProvider>
   );
 }
 
