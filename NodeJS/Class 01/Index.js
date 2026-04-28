559541dbdfcf3af4b3a2d08403304cc55f3abe4f
AdilAhmedShekhani <adilaziz2009@live.com>
2026-04-28 21:42:05 +0500

    Get Request Created

--- NodeJS/Class 01/Index.js
+++ NodeJS/Class 01/Index.js
@@ -2,6 +2,13 @@ import express from "express";
 
 const app = express();
 
+app.get("/", (req, res) => {
+  res.send("Hello Students");
+});
+app.get("/users", (req, res) => {
+  res.send([{ name: "Adil Ahmed Shekhani" }]);
+});
+
 app.listen(3000, () => {
   console.log("Server is running on 3000");
 });
