73d6a4863b224466ae0391a4abf1097e40d7acac
AdilAhmedShekhani <adilaziz2009@live.com>
2026-05-03 23:28:02 +0500

    Rest API Created

--- NodeJS/Class 02/index.js
+++ NodeJS/Class 02/index.js
@@ -5,7 +5,22 @@ const PORT = 4000;
 
 app.get("/", (req, res) => {
   console.log(req);
-  res.send("Hello World to first API Created");
+  res.send("Get Request Created");
+});
+
+app.post("/", (req, res) => {
+  console.log(req);
+  res.send("Post Request is Called");
+});
+
+app.put("/", (req, res) => {
+  console.log(req);
+  res.send("put Request is Called");
+});
+
+app.delete("/", (req, res) => {
+  console.log(req);
+  res.send("delete Request is Called");
 });
 
 app.listen(PORT, () => console.log("Server is Running on Port " + PORT));
