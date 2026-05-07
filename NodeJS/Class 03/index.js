ecf2851b6c87efec46fe28850f67be46cb999399
UzairShekhani <uzairshekhani2017@gmail.com>
2026-05-07 23:31:14 +0500

    Route Added on main page

--- NodeJS/Class 03/index.js
+++ NodeJS/Class 03/index.js
@@ -1,2 +1,18 @@
 
-console.log("Hello, World!");
+
+import express from "express";
+import userRouter from "./routers/users.js";
+
+const app = express();
+app.use("/users", userRouter);
+
+app.get("/", (req, res) => {
+  res.status(200).json({
+    error: false,
+    message: "Welcome to the API",
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
